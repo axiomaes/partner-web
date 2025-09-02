5ccf5612c82d94877f0ecd5eeeb8d24358b198c5
Axioma Creativa <ia@axioma-creativa.es>
2025-09-02 14:38:04 +0200

    Delete src/portal/portal.module.ts

--- src/portal/portal.module.ts
+++
@@ -1,9 +0,0 @@
-// src/portal/portal.module.ts
-import { Module } from '@nestjs/common';
-import { CustomerPortalController } from './portal.controller'; // tu archivo actual con guards
-import { PortalPublicController } from './portal.public.controller'; // nuevo público
-
-@Module({
-  controllers: [CustomerPortalController, PortalPublicController],
-})
-export class PortalModule {}
