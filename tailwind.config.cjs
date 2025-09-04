5aa70d2aface77d7d2570fb6690e2d8f2420cfd0
Axioma Creativa <ia@axioma-creativa.es>
2025-09-04 15:10:14 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [require("daisyui")],
-  // Si tienes un tema 'lacubierta', defínelo aquí; si no, usa los por defecto
-  // daisyui: { themes: ["light", "dark", "cupcake"] },
+  daisyui: { themes: ["light", "dark", "cupcake"] },
 };
